3d741e87a373606edbf3e617b221507fa4773fe7
Andrew Norris <norrisa@faytechcc.edu>
2021-09-15 11:32:21 -0400

    New added code
    I made some changes but it's fine I'm sure

--- P1T2_Norris.py
+++ P1T2_Norris.py
@@ -24,3 +24,6 @@ userNum = int(input())
 userNumSquared = userNum + userNum   # Bug here; fix it when instructed
    
 print(userNumSquared, end=' ')       # Output formatting issue here; fix it when instructed
+
+print ("That's all"
+    

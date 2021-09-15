5ecc78fb5fc263bf66da98ffb0b8aeb4f0bf2746
Andrew Norris <norrisa@faytechcc.edu>
2021-09-15 11:34:55 -0400

    Fixed bug with last line

--- P1T2_Norris.py
+++ P1T2_Norris.py
@@ -25,5 +25,5 @@ userNumSquared = userNum + userNum   # Bug here; fix it when instructed
    
 print(userNumSquared, end=' ')       # Output formatting issue here; fix it when instructed
 
-print ("That's all"
+print ("That's all")
     

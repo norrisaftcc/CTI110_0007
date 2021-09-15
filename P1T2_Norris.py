722bf2dbfb612a17f51fe1e69ef360fe0690d2f1
Andrew Norris <norrisa@faytechcc.edu>
2021-09-15 11:30:35 -0400

    Add files via upload

---
+++ P1T2_Norris.py
@@ -0,0 +1,26 @@
+# CTI 110
+# P1T2 - Salary Calculator
+# Name
+# Date
+
+# Start
+"""
+# Input the hours worked
+hoursWorked = float(input("How many hours did you work this week? "))
+#hoursWorked = int(hoursWorked) # convert to integer
+
+# Input the hourly pay rate
+hourlyPay = float(input("What's your hourly pay rate? ")) # get and convert
+
+# Calculate gross pay (hours worked times pay rate)
+grossPay = hoursWorked * hourlyPay
+
+# Display the gross pay
+print ("Your gross pay for the week is: $", grossPay)
+
+# End
+"""
+userNum = int(input())
+userNumSquared = userNum + userNum   # Bug here; fix it when instructed
+   
+print(userNumSquared, end=' ')       # Output formatting issue here; fix it when instructed

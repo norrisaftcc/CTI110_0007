526a195f2f2dd37f5b5ec4acfc40df5100a714e5
Andrew Norris <norrisa@faytechcc.edu>
2021-09-15 11:30:06 -0400

    First upload

---
+++ p1lab1_lastname.py
@@ -0,0 +1,10 @@
+# CTI 110
+# P1LAB1 - zylab training
+# Name
+# Date
+
+
+userNum = int(input())
+userNumSquared = userNum * userNum   # Bug here; fixed
+   
+print(userNumSquared)       # Output formatting issue here; fixed
